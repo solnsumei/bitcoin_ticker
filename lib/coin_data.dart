696199dca231c95a2c47fcd0f4368a4bfccb0352
solnsumei <solnsumei@gmail.com>
2020-06-30 22:06:59 +0100

    Completed challenge

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = 'BD0013F9-6C40-404C-BC49-F985B85B5E5D';
+const apiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future<dynamic> getCoinData({String coinName='BTC', String currency='USD'}) async {
+    try {
+      final http.Response response = await http.get('$apiUrl/$coinName/$currency', headers: {
+        'X-CoinAPI-Key': apiKey,
+      });
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show TargetPlatform;
+import 'coin_data.dart';
+import 'coin_data.dart';
+
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +11,123 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  final CoinData coinDataHelper = CoinData();
+  String btcRate;
+  String ethRate;
+  String ltcRate;
+
+  initState() {
+    super.initState();
+    getCoinData();
+  }
+
+  void getCoinData() async {
+    var btcData, ltcData, ethData;
+
+    try {
+      await Future.wait<void>([
+        (() async => btcData = await coinDataHelper.getCoinData(coinName: 'BTC', currency: selectedCurrency))(),
+        (() async => ethData = await coinDataHelper.getCoinData(coinName: 'ETH', currency: selectedCurrency))(),
+        (() async => ltcData = await coinDataHelper.getCoinData(coinName: 'LTC', currency: selectedCurrency))()
+      ]);
+
+      if (!mounted) return;
+      if (btcData != null) {
+        btcRate = '${(btcData['rate']).toInt()} $selectedCurrency';
+      }
+
+      if (ethData != null) {
+        ethRate = '${(ethData['rate']).toInt()} $selectedCurrency';
+      }
+
+      if (ltcData != null) {
+        ltcRate = '${(ltcData['rate']).toInt()} $selectedCurrency';
+      }
+
+      setState(() {});
+
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  List<dynamic> getMenuItems({isIOS = false}) {
+    return currenciesList.map((item) {
+      return isIOS
+          ? Text(item, style: TextStyle(color: Colors.white),)
+          : DropdownMenuItem<String>(value: item, child: Text(item),
+      );
+    }).toList();
+  }
+
+  Widget androidDropdown() {
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      onChanged: (value) {
+        // print(value);
+        setState(() {
+          selectedCurrency = value;
+        });
+
+        getCoinData();
+      },
+      items: currenciesList.map((item) {
+        return DropdownMenuItem<String>(value: item, child: Text(item),);
+      }).toList(),
+    );
+  }
+
+  Widget iOSPicker() {
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+
+        getCoinData();
+      },
+      children: currenciesList.map((item) {
+        return Text(item, style: TextStyle(color: Colors.white),);
+      }).toList(),
+
+    );
+  }
+
+  List<Widget> currencyCards() {
+
+    return cryptoList.map((item) {
+      final crytoRateMap = {
+        'BTC': btcRate,
+        'ETH': ethRate,
+        'LTC': ltcRate,
+      };
+
+      return Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              '1 $item = ${crytoRateMap[item] ?? '? USD'}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,36 +135,18 @@ class _PriceScreenState extends State<PriceScreen> {
         title: Text('🤑 Coin Ticker'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          ...currencyCards(),
+          Spacer(),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Theme.of(context).platform == TargetPlatform.iOS
+                ? iOSPicker()
+                : androidDropdown(),
           ),
         ],
       ),
